4b6f276535e440257c7e3ca805d9db98158377a6
Ties Klappe <tg.klappe@gmail.com>
2023-06-07 14:18:48 +0200

    Remove log

--- analysis/analysis.ts
+++ analysis/analysis.ts
@@ -125,7 +125,6 @@ export function getRedirections() {
         // Collect all data into a filtered redirect map.
         redirectsUnique.forEach((redirect, redirectStr) => {
             if (filteredRedirects.has(redirectStr)) {
-                console.log("INCREMENT FOR STR " + redirectStr);
                 let object = filteredRedirects.get(redirectStr);
                 object['distinctWebsites'] = object['distinctWebsites'] + 1;
                 filteredRedirects.set(redirectStr, object);
